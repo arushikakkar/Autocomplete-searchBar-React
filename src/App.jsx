616bb27d0faf88265fa8fda1685f171c896fba4d
arushikakkar <aarushikakkarr31@gmail.com>
2025-07-14 17:11:03 +0530

    debounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./style.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [input, setInput] = useState(""); // State to hold the input value
+  const [results, setResults] = useState([]); // State to hold the fetched results
+  const [showResults, setShowResults] = useState(false); // State to control the visibility of results
+
+  const fetchData = async () => {
+    console.log("api call", input); // Log the input to see the API call
+    const data = await fetch("https://dummyjson.com/recipes/search?q=" + input); // Fetch data from the API
+    const json = await data.json(); // Parse the JSON response
+    setResults(json?.recipes); // Update the results state with the fetched recipes
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(fetchData, 400); //debounce the API call
+    return () => {
+      clearTimeout(timer); // cleanup function to clear the timer
+    };
+  }, [input]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h1>AutoComplete Search Bar</h1>
+      <input
+        type="text"
+        className="search"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onFocus={() => setShowResults(true)} // Show results when input is focused
+        onBlur={() => setShowResults(false)} // Hide results when input loses focus
+      />
 
-export default App
+      {showResults && (  // Render results only when showResults is true
+        <div className="result-container">
+          {results.map((r) => (  // Map through the results and display each one
+            <span className="result" key={r.id}>  
+              {r.name}
+            </span>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
